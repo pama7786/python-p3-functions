3466e69b566d08bf3ce89bf2432904b9d6839e99
abdirahman-abass-diss <abdirahman.diss@student.moringaschool.com>
2023-12-18 21:22:34 +0300

    finished

--- lib/functions.py
+++ lib/functions.py
@@ -14,3 +14,32 @@ def add(num1, num2):
 
 def halve(number):
     pass
+# In lib/functions.py
+
+def greet_programmer():
+    print("Hello, programmer!")
+
+def greet_programmer():
+    greeting = "Hello, programmer!"
+    print(greeting)
+    return greeting
+
+def greet(name):
+    greeting = f"Hello, {name}!"
+    print(greeting)
+    return greeting
+
+def greet_with_default(name="programmer"):
+    greeting = f"Hello, {name}!"
+    print(greeting)
+    return greeting
+
+def add(num1, num2):
+    result = num1 + num2
+    print(f"{num1} + {num2} = {result}")
+    return result
+
+def halve(number):
+    result = number / 2
+    print(f"Half of {number} is {result}")
+    return result
